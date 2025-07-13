eda733a73654205ee28f5789306cedc01873de85
rennon21 <95376888+rennon21@users.noreply.github.com>
2025-07-13 19:10:26 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ import "bootstrap";
 import "./style.css";
 
 
+
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function generarExcusas() {
+  const sujetos = ["Mi compañero", "Mi hermano", "Mi profesor", "Mi perro"];
+  const accion = ["perdio", "rompio","entrego", "vio"];
+  const objeto =["el carro", "la tarea","la pelicula","el ordenador"];
+  const cuando = ["la semana pasada", "el lunes","en el aula de clases", "el mes pasado"];
+
+  const randomsujeto = sujetos[Math.floor(Math.random() * sujetos.length)];
+  const randomacciones = accion[Math.floor(Math.random() * accion.length)];
+  const randomobjetos = objeto[Math.floor(Math.random() * objeto.length)];
+  const randomcuandos = cuando[Math.floor(Math.random() * cuando.length)];
+
+  const excusas = `${randomsujeto} ${randomacciones} ${randomobjetos} ${randomcuandos}`;
+  document.getElementById("excusas").textContent = excusas;
+  
+  console.log(randomsujeto, randomacciones, randomobjetos, randomcuandos);
+}
+  
   console.log("Hello Rigo from the console!");
-};
+
